1cc4f379fe85f63f7b8f592a419eaef42dfadff4
nilesh <Nilesh123@gmail.com>
2024-08-27 15:32:02 +0530

    project

---
+++ nilesh/src/main/java/com/example/nilesh/NileshApplication.java
@@ -0,0 +1,13 @@
+package com.example.nilesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NileshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NileshApplication.class, args);
+	}
+
+}
